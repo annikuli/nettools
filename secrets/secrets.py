a578da29db316c623e17785b820b63a690d0b2e2
mr515am <nikulin.anthony@gmail.com>
2017-04-13 15:20:00 +0500

    mail list changed

--- secrets/secrets.py
+++ secrets/secrets.py
@@ -4,5 +4,8 @@ ZABBIX_USERNAME = "ZabbixAPI"
 ZABBIX_PASSWORD = "zabbixpass"
 DEFAULT_SNMP_COMMUNITY = "Zorkiy_glas_vidit_mnogoe"
 ENABLED = False
-ZABBIX_EMAIL_DESTINATIONS = ['nikulinad@bashkirenergo.ru']
+ZABBIX_EMAIL_DESTINATIONS = ['nikulinad@bashkirenergo.ru',
+                             'AkhmadullinaIR@bashkirenergo.ru',
+                             'KokushevKK@bashkirenergo.ru',
+                             'TrunovGV@bashkirenergo.ru']
 ZABBIX_EMAIL_SOURCE = 'zabbix@bashkirenergo.ru'
